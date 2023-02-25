addaf070df4918c2178ebdff4bca793f59f25e03
Boodevil <125692951+Boodevil@users.noreply.github.com>
2023-02-25 09:23:36 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -11,15 +11,6 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text="Wanna me?",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="Help",
-                callback_data="settings_back_helper",
-            ),
-            InlineKeyboardButton(
-                text="Settings", callback_data="settings_helper"
             ),
         ],
      ]
@@ -32,19 +23,6 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text="Wanna me?",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="Support", url=config.SUPPORT_GROUP
-            ),
-            InlineKeyboardButton(
-                text="Updates", url=config.SUPPORT_CHANNEL
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                text="Help&Commands", callback_data="settings_back_helper"
             ),
         ],
      ]
